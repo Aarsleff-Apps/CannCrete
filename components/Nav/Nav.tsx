0c34d841feaba7ac83bb9888054880647dd425df
bb-centrum <benbridges@centrumpile.co.uk>
2024-05-10 14:47:53 +0100

    📝 (Nav.module.css): remove unused CSS classes and adjust icon size in .icon class ✨ (Nav.tsx): add functionality to toggle color scheme between light and dark mode using ActionIcon and IconSun/IconMoon components

--- components/Nav/Nav.tsx
+++ components/Nav/Nav.tsx
@@ -1,8 +1,12 @@
-import { AppShell, Group, Image, Title } from "@mantine/core";
+import { ActionIcon, AppShell, Group, Image, Title, useComputedColorScheme, useMantineColorScheme } from "@mantine/core";
 import NextImage from "next/image";
 import CannonLogo from "/public/android-chrome-512x512.png";
-
+import classes from "./Nav.module.css";
+import { IconMoon, IconSun } from "@tabler/icons-react";
+import cx from "clsx";
 export default function Nav({ children }: { children: React.ReactNode }) {
+    const { setColorScheme } = useMantineColorScheme();
+  const computedColorScheme = useComputedColorScheme();
   return (
     <AppShell padding="md" header={{ height: 60 }}>
       <AppShell.Header>
@@ -15,6 +19,10 @@ export default function Nav({ children }: { children: React.ReactNode }) {
               h={30}
             />
             <Title order={3}>CannCrete</Title>
+            <ActionIcon variant="default" aria-label="Settings" onClick={() => setColorScheme(computedColorScheme === "light" ? "dark" : "light")}>
+                <IconSun className={cx(classes.light, classes.icon)} stroke={1.5}/>
+                <IconMoon className={cx(classes.dark, classes.icon)} stroke={1.5}/>
+            </ActionIcon>
           </Group>
         </Group>
       </AppShell.Header>
